60857c020d652246879a6c48447609828d347195
Dicmen92 <babich-d@mail.ru>
2024-08-03 17:16:04 +0500

    feat: add code in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let res = "";
+    n = String(String(n).split(", ")).split("");
+    for (let i = n.length - 1; i >= 0; i--) {
+        res += n[i];
+    }
+    return +res.split(" ").join("");
+};
+
+// function reverse(n) {}
+
+// console.log(reverse(12345));
